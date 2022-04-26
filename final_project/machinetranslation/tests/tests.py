fb5e54ba99294ea2fdb6001bd2b1cba9e6665ec8
Adilson Silva <adilson.thanus@gmail.com>
2022-04-26 11:09:29 +0000

    add text to tests

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -7,27 +7,27 @@ from machinetranslation import translator
 
 class TestTranslator(unittest.TestCase): 
     def test_french_to_english_assert_equal(self): 
-        self.assertEqual(translator.french_to_english('Bonjour'), 'Hello') 
+        self.assertEqual(translator.french_to_english('Bonjour'), 'Hello', "frenchToEnglish - assertEqual") 
 
     def test_french_to_english_assert_not_equal(self): 
-        self.assertNotEqual(translator.french_to_english('Bonjour'), 'Hi') 
+        self.assertNotEqual(translator.french_to_english('Bonjour'), 'Hi', "frenchToEnglish - assertNotEqual") 
 
     def test_french_to_english_empty(self): 
-        self.assertIsNone(translator.french_to_english('')) 
+        self.assertIsNone(translator.french_to_english(''), "frenchToEnglish - Empty") 
 
     def test_french_to_english_null(self): 
-        self.assertIsNone(translator.french_to_english(None)) 
+        self.assertIsNone(translator.french_to_english(None), "frenchToEnglish - Null") 
 
     def test_english_to_french_assert_equal(self): 
-        self.assertEqual(translator.english_to_french('Hello'), 'Bonjour') 
+        self.assertEqual(translator.english_to_french('Hello'), 'Bonjour', "englishToFrench - assertEqual") 
 
     def test_english_to_french_assert_not_equal(self): 
-        self.assertNotEqual(translator.english_to_french('Hello'), 'Hallo') 
+        self.assertNotEqual(translator.english_to_french('Hello'), 'Hallo', "englishToFrench - assertNotEqual") 
 
     def test_english_to_french_empty(self): 
-        self.assertIsNone(translator.english_to_french('')) 
+        self.assertIsNone(translator.english_to_french(''), "englishToFrench - Empty") 
 
     def test_english_to_french_null(self): 
-        self.assertIsNone(translator.english_to_french(None)) 
+        self.assertIsNone(translator.english_to_french(None), "englishToFrench - assertNull") 
 
 unittest.main()
