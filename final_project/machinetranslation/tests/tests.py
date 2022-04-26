d39deb277a531143539e9a16dbc517fdc83dfad0
Adilson Silva <adilson.thanus@gmail.com>
2022-04-26 10:46:36 +0000

    add test not equal

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -6,17 +6,23 @@ sys.path.append('../..')
 from machinetranslation import translator
 
 class TestTranslator(unittest.TestCase): 
-    def test_french_to_english(self): 
+    def test_french_to_english_assert_equal(self): 
         self.assertEqual(translator.french_to_english('Bonjour'), 'Hello') 
+
+    def test_french_to_english_assert_not_equal(self): 
+        self.assertNotEqual(translator.french_to_english('Bonjour'), 'Hi') 
 
     def test_french_to_english_empty(self): 
         self.assertIsNone(translator.french_to_english('')) 
 
     def test_french_to_english_null(self): 
-        self.assertIsNone(translator.french_to_english('')) 
+        self.assertIsNone(translator.french_to_english(None)) 
 
-    def test_english_to_french(self): 
+    def test_english_to_french_assert_equal(self): 
         self.assertEqual(translator.english_to_french('Hello'), 'Bonjour') 
+
+    def test_english_to_french_assert_not_equal(self): 
+        self.assertNotEqual(translator.english_to_french('Hello'), 'Hallo') 
 
     def test_english_to_french_empty(self): 
         self.assertIsNone(translator.english_to_french('')) 
